c5534793fcb206b17eb201e3df2614404acdb7e7
angusmcleod <angus@mcleod.org.au>
2021-08-11 16:24:51 +0800

    Text and formatting updates

--- javascripts/discourse/initializers/theme-javascript.js
+++ javascripts/discourse/initializers/theme-javascript.js
@@ -36,6 +36,7 @@
 *              ```
 *              app/assets/javascripts/discourse/app/lib/plugin-api.js
 *              ```
+*              ([GitHub link](https://github.com/discourse/discourse/blob/main/app/assets/javascripts/discourse/app/lib/plugin-api.js))
 *
 *              Open this file now in your code editor and scroll through it.
 *              You'll see a number of methods, each with a comment block above
@@ -51,7 +52,7 @@
 *              create your theme), and gives you an ``api`` object in the
 *              callback you can use to access the client side API methods.
 *
-*              When you're done looking through the Plugin API see if you can
+* exercise:    When you're done looking through the Plugin API see if you can
 *              figure out how to make Clippy do the following by moving the
 *              Clippy initialization from the ``body_tag`` to the initializer,
 *              and using the plugin api methods in the ``withPluginApi`` callback
@@ -61,40 +62,15 @@
 *              - Tell you what the title of the page is when it changes.
 *
 *              You'll find some clues in the theme components referenced back in
-*              Step 1.
+*              Step 1 of this unit.
 */
 import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default {
   name: "theme-javascript-initializer",
   initialize() {
-    let clippyAgent;
-
-    clippy.load('Clippy', function(agent){
-      clippyAgent = agent;
-      clippyAgent.show();
-      clippyAgent.speak("Hey there, welcome to our Discourse! I'm Clippy");
-    });
-
     withPluginApi("0.8.30", api => {
-      api.onPageChange((url, title) => {
-        if (clippyAgent) {
-          clippyAgent.speak(`Looks like you changed the page to ${title}`);
-        }
-      });
 
-      api.onAppEvent('composer:opened', () => {
-        clippyAgent.speak(`Oooo a bold move! Let's see what you have to say`);
-      });
-
-      api.composerBeforeSave(function() {
-        if (this.reply.toLowerCase().includes("i hate clippy")) {
-          clippyAgent.speak(`That's a terrible thing to say! Can't let you post that.`);
-          return Promise.reject();
-        } else {
-          return Promise.resolve();
-        }
-      });
     });
   }
 };
@@ -119,6 +95,7 @@ export default {
 *              ```
 *              app/javascripts/discourse/app
 *              ```
+*              ([GitHub link](https://github.com/discourse/discourse/blob/main/app/javascripts/discourse/app))
 *
 *              This will give you a list of results that look like this
 *
@@ -131,9 +108,9 @@ export default {
 *              being a callback we can attach to the event. Our callback will
 *              get whatever arguments are passed to the event trigger.
 *
-*              See if you can use the client side event bus to make Clippy say
-*              something whenever you open the composer. Then see if you can make
-*              him perform different animations when you type something in the
+* exercise:    Use the client side event bus to make Clippy say something
+*              whenever you open the composer. Then see if you can make him
+*              perform different animations when you type something in the
 *              composer (as he sometimes used to do in MS Word).
 */
 /* /learning_step */
@@ -154,17 +131,17 @@ export default {
 *
 *              Because of this, whenever you're customising something in
 *              Discourse javascript, even if you're quite experienced, it's a
-*              good idea to thoroughly re-check the available API methods before
-*              you consider using the "fallback" API method ``modifyClass`` which
+*              good idea to re-check the available API methods before you
+*              consider using the "generic" API method ``modifyClass`` which
 *              allows you to modify nearly any javascript in Discourse. We'll
 *              cover that approach in the next unit(s), however if there's a more
 *              specific method available, one that targets your use case, it's
 *              best to use that.
 *
-*              See if you can find a method in the api that will let you check
-*              whether a post contains the words "I hate Clippy", then prevent
-*              such a post from being posted, and get Clippy to say a disapproving
-*              message in response. You'll need to use some of the codebase
-*              searching skills we covered in previous units.
+* exercise:    Find a method in the api that will let you check whether a post
+*              contains the words "I hate Clippy", then prevent such a post from
+*              being posted, and get Clippy to say a disapproving message in
+*              response. You'll need to use some of the codebase searching skills
+*              we covered in previous units.
 */
 /* /learning_step */
